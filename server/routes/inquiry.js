769afc9d61b02765296f256e1c345d065244a572
joy0215 <11056017@ntub.edu.tw>
2026-02-22 02:50:32 +0800

    fix: 捨棄 service:gmail，修復 Render IPv6 寄信連線逾時問題

--- server/routes/inquiry.js
+++ server/routes/inquiry.js
@@ -5,7 +5,9 @@ const nodemailer = require('nodemailer');
 const authMiddleware = require('../middleware/auth'); 
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
